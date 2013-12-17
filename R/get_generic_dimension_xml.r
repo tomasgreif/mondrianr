0c3b77ae16c324a4679ac54c120c919f15d09fcc
Tomáš Greif <tgr>
2013-12-17 10:43:11 +0100

    added control - there has to be at least one generic or date dimension.

--- R/get_generic_dimension_xml.r
+++ R/get_generic_dimension_xml.r
@@ -23,6 +23,22 @@ get_generic_dimension_xml <- function(engine, final_design, table, time_table=NA
   
   dimension   <- final_design[final_design$dimension, ]
   dimensions  <- nrow(dimension)
+  dimension_xml <- character(0)
+  
+  time_dimension  <- final_design[final_design$class=='date' & final_design$dimension,]
+  time_dimensions <- nrow(time_dimension)
+  
+  if(dimensions==0 & time_dimensions == 0) {
+    if(debug) {
+      cat('Your call will result in error (wait for it :), so here you have final design to better understand why this happens. \n')
+      print(final_design)
+    }    
+    stop("
+      There are no generic dimensions and no time dimensions. This won't work. Check what is in the table and try again.
+      To fix it, you probably need to change default mapping, table layout (try ?get_table_design) or dimension argument in
+      create_schema() if used. If you run your call again with debug=TRUE, the function will print final design.
+      See the above generated final design to analyze why there are no dimensions.")
+  }
 
   if(dimensions > 0) {
     table_name <- parse_table_name(engine, table)
